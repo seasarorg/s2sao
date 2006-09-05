56ccc9497dd861b9f0b4e80cdf4ba296b38a1a86
bowez <bowez@57f59297-a467-4629-a757-7e5675632957>
2006-09-05 17:45:18 +0000

    [jruby]S2の名前空間に対応
    git-svn-id: https://www.seasar.org/svn/sandbox/s2sao/trunk@19 57f59297-a467-4629-a757-7e5675632957

--- s2sao/src/main/org/seasar/sao/jruby/JRubyScriptEngine.java
+++ s2sao/src/main/org/seasar/sao/jruby/JRubyScriptEngine.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.seasar.sao.jruby;
 
 import java.io.BufferedReader;
@@ -19,6 +34,10 @@ import org.seasar.sao.ScriptEngine;
 
 import static org.seasar.sao.ScriptEngineUtil.*;
 
+/**
+ * 
+ * @author bowez
+ */
 public class JRubyScriptEngine implements ScriptEngine {
     private static final long serialVersionUID = 8821201315656298440L;
 
@@ -29,87 +48,81 @@ public class JRubyScriptEngine implements ScriptEngine {
         this.container = container;
     }
 
-	public IRuby getRuntime() {
-		return jRubyRuntime;
-	}
+    public IRuby getRuntime() {
+        return jRubyRuntime;
+    }
 
-	public void setRuntime(IRuby rubyRuntime) {
-		if (jRubyRuntime == null) {
-			throw new IllegalArgumentException("JRubyRuntime cannot be null.");
-		}
-		jRubyRuntime = rubyRuntime;
-	}
+    public void setRuntime(IRuby rubyRuntime) {
+        if (jRubyRuntime == null) {
+            throw new IllegalArgumentException("JRubyRuntime cannot be null.");
+        }
+        jRubyRuntime = rubyRuntime;
+    }
 
     protected String getScriptFilePath(Class<?> sao) {
-		if (sao == null) {
-			throw new IllegalArgumentException();
-		}
-		if (sao.isAnnotationPresent(ScriptBinding.class)) {
-			ScriptBinding binding = sao.getAnnotation(ScriptBinding.class);
-			String value = binding.value();
-			if (value.length() > 0) {
-				return value;
-			}
-		}
-		String path = sao.getName();
-		path = path.replace('.', '/') + ".rb";
-		return path;
-	}
-
-	public Object compile(Class<?> sao) throws Exception {
-		String path = getScriptFilePath(sao);
-		InputStream stream = getScriptStream(path);
-		if (stream != null) {
-			try {
-				BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-                jRubyRuntime.eval(jRubyRuntime.parse(reader, path));
-                IRubyObject main = getTopSelf(jRubyRuntime);
-				return main;
-			}
-			finally {
-				stream.close();
-			}
-		}
-		return null;
-	}
-
-	public boolean hasFunction(Object compiled, Method method) {
-		IRubyObject main = IRubyObject.class.cast(compiled);
-		return main.respondsTo(getScriptMethodName(method));
-	}
-
-	public Object invoke(Object compiled, Method method, Object[] args,
-			Class<?> expectedClass) throws Throwable {
-		IRubyObject main = IRubyObject.class.cast(compiled);
-		IRubyObject[] rubyArgs = JavaUtil.convertJavaArrayToRuby(jRubyRuntime, args);
-		IRubyObject result = main.callMethod(getScriptMethodName(method), rubyArgs);
-		
-		return JavaUtil.convertRubyToJava(result);
-	}
-
-    protected IRubyObject getTopSelf(IRuby runtime) {
-        IRubyObject main = jRubyRuntime.getTopSelf();
-        RubyModule s2module = getS2ContainerModule(runtime);
-        if (!main.isKindOf(s2module)) {
-            main.extendObject(s2module);
+        if (sao == null) {
+            throw new IllegalArgumentException();
         }
-        return main;
+        if (sao.isAnnotationPresent(ScriptBinding.class)) {
+            ScriptBinding binding = sao.getAnnotation(ScriptBinding.class);
+            String value = binding.value();
+            if (value.length() > 0) {
+                return value;
+            }
+        }
+        String path = sao.getName();
+        path = path.replace('.', '/') + ".rb";
+        return path;
+    }
+
+    public Object compile(Class<?> sao) throws Exception {
+        String path = getScriptFilePath(sao);
+        InputStream stream = getScriptStream(path);
+        if (stream != null) {
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+                jRubyRuntime.eval(jRubyRuntime.parse(reader, path));
+                IRubyObject main = jRubyRuntime.getTopSelf();
+                RubyModule s2module = getS2ContainerModule(jRubyRuntime, container);
+                if (!main.isKindOf(s2module)) {
+                    main.extendObject(s2module);
+                }
+                return main;
+            }
+            finally {
+                stream.close();
+            }
+        }
+        return null;
+    }
+
+    public boolean hasFunction(Object compiled, Method method) {
+        IRubyObject main = IRubyObject.class.cast(compiled);
+        return main.respondsTo(getScriptMethodName(method));
+    }
+
+    public Object invoke(Object compiled, Method method, Object[] args,
+            Class<?> expectedClass) throws Throwable {
+        IRubyObject main = IRubyObject.class.cast(compiled);
+        IRubyObject[] rubyArgs = JavaUtil.convertJavaArrayToRuby(jRubyRuntime, args);
+        IRubyObject result = main.callMethod(getScriptMethodName(method), rubyArgs);
+        return JavaUtil.convertRubyToJava(result);
     }
     
-    RubyModule getS2ContainerModule(IRuby runtime) {
-        RubyModule module = runtime.getModule("S2Continer");
+    protected RubyModule getS2ContainerModule(IRuby runtime, S2Container container) {
+        RubyModule module = runtime.getModule("S2Container");
         if (module == null) {
             module = runtime.defineModule("S2Container");
             module.defineModuleFunction("method_missing", 
-                    new MethodMissingHandler(container, jRubyRuntime));
+                    new TopSelfMethodMissingHandler(container, runtime));
         }
         return module;
     }
     
-    static class MethodMissingHandler implements Callback {
+    static class TopSelfMethodMissingHandler implements Callback {
         final S2Container container;
         final IRuby runtime;
-        MethodMissingHandler(S2Container container, IRuby runtime) {
+        TopSelfMethodMissingHandler(S2Container container, IRuby runtime) {
             this.container = container;
             this.runtime = runtime;
         }
@@ -119,9 +132,9 @@ public class JRubyScriptEngine implements ScriptEngine {
                 if (container.hasComponentDef(name)) {
                     Object component = container.getComponent(name);
                     IRubyObject obj = JavaUtil.convertJavaToRuby(runtime, component);
-                    RubyModule javaBridge = getJavaMethodCallable(runtime, component);
-                    if (!obj.isKindOf(javaBridge)) {
-                        obj.extendObject(javaBridge);
+                    RubyModule proxy = ComponentMethodMissing.getS2ComponentProxy(runtime);
+                    if (!obj.isKindOf(proxy)) {
+                        obj.extendObject(proxy);
                     }
                     return obj;
                 }
@@ -132,30 +145,44 @@ public class JRubyScriptEngine implements ScriptEngine {
         public Arity getArity() {
             return Arity.required(1);
         }
+    }
+    
+    static class ComponentMethodMissing implements Callback {
+        final IRuby runtime;
         
-        RubyModule getJavaMethodCallable(IRuby runtime, Object component) {
-            RubyModule module = runtime.getModule("JavaMethodCallable");
+        ComponentMethodMissing(IRuby runtime) {
+            this.runtime = runtime;
+        }
+
+        static RubyModule getS2ComponentProxy(IRuby runtime) {
+            RubyModule module = runtime.getModule("S2ComponentProxy");
             if (module == null) {
-                module = runtime.defineModule("JavaMethodCallable");
-                module.defineModuleFunction("method_missing", new JavaMethodCallOnMethodMissing(runtime));
+                module = runtime.defineModule("S2ComponentProxy");
+                module.defineModuleFunction("method_missing", new ComponentMethodMissing(runtime));
             }
             return module;
         }
-    }
-    
-    static class JavaMethodCallOnMethodMissing implements Callback {
-        final IRuby runtime;
-        JavaMethodCallOnMethodMissing(IRuby runtime) {
-            this.runtime = runtime;
-        }
+        
         public IRubyObject execute(IRubyObject recv, IRubyObject[] args) {
             if (args.length < 1) {
                 return recv.callMethod("method_missing", args);
             }
+            String name = args[0].asSymbol();
             JavaObject javaObject = (JavaObject) recv;
-            String methodName = args[0].asSymbol();
+            if (javaObject.getValue() instanceof S2Container) {
+                S2Container container = (S2Container) javaObject.getValue();
+                if (container.hasComponentDef(name)) {
+                    Object component = container.getComponent(name);
+                    IRubyObject obj = JavaUtil.convertJavaToRuby(runtime, component);
+                    RubyModule proxy = getS2ComponentProxy(runtime);
+                    if (!obj.isKindOf(proxy)) {
+                        obj.extendObject(proxy);
+                    }
+                    return obj;
+                }
+            }
             Class[] argTypes = getArgTypes(args);
-            Method method = loadMethod(recv.getJavaClass(), methodName, argTypes);
+            Method method = loadMethod(recv.getJavaClass(), name, argTypes);
             if (method == null) {
                 return recv.callMethod("method_missing", args);
             }
